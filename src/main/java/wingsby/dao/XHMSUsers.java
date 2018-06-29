e7e11188ab6587383ed7d7c3bc45d8765e59aab6
momogoutou <910139735@qq.com>
2018-06-29 16:34:35 +0800

    2018/06/06 wangye v0.1.0 增加注册口令和非对称加密

---
+++ src/main/java/wingsby/dao/XHMSUsers.java
@@ -0,0 +1,59 @@
+package wingsby.dao;
+
+public class XHMSUsers {
+    String name;
+    String passwd;
+    String pubKey;
+    String priKey;
+    int status;
+    int id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getPubKey() {
+        return pubKey;
+    }
+
+    public void setPubKey(String pubKey) {
+        this.pubKey = pubKey;
+    }
+
+    public String getPriKey() {
+        return priKey;
+    }
+
+    public void setPriKey(String priKey) {
+        this.priKey = priKey;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
